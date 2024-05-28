9ae916e0db328f11e0b6956d1e463c5870142abb
dcChilshock <densonchilders123@gmail.com>
2024-05-28 14:36:04 -0500

    hello

--- SilkyFlow/Form2.cs
+++ SilkyFlow/Form2.cs
@@ -34,17 +34,42 @@ namespace SilkyFlow
         int lcv = 0;
         private void button2_Click(object sender, EventArgs e)
         {
+            int set1 = 1;
+            int set2 = 2;
+            int set3 = 3;
+            int set4 = 4;
+            int set5 = 5;
+            label1.Visible = false;
+            if (lcv >= 5)
+                lcv = 0;
             lcv++;
-            if (lcv == 1)
+            if (lcv == set1)
+            {
                 textBox1.Visible = true;
-            else if (lcv == 2)
+                set1++;
+            }
+                
+            else if (lcv == set2)
+            {
                 textBox2.Visible = true;
-            else if (lcv == 3)
+                set2++;
+            }
+                
+            else if (lcv == set3)
+            {
                 textBox3.Visible = true;
-            else if (lcv == 4)
+            }
+                
+            else if (lcv == set4)
+            {
                 textBox4.Visible = true;
-            else if (lcv == 5)
+            }
+                
+            else if (lcv == set5)
+            {
                 textBox5.Visible = true;
+            }
+                
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -56,7 +81,16 @@ namespace SilkyFlow
         {
             label1.Visible = true;
             textBox1.Visible = false;
-            label1.Text = "1: " + textBox1.Text + "\n\n" + "2: " + textBox2.Text;
+            label1.Text = "1: " + textBox1.Text + "\n\n" + "2: " + textBox2.Text + "\n\n" + "3: " + textBox3.Text + "\n\n" + "4: " + textBox4.Text + "\n\n" + "5: " + textBox5.Text;
+            Properties.Settings.Default.label1 = label1.Text;
+            Properties.Settings.Default.Save();
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label1.Visible = true;
+            label1.Text = Properties.Settings.Default.label1;
         }
     }
 }
